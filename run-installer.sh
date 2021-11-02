9d7a7473d8bae8722e2db9cfb4b1bcdb053acd99
DecentAgeCoder <marco.oesch@decentage.com>
2021-11-02 11:58:41 +0100

    remove existing installation when running run-installer.sh

--- run-installer.sh
+++ run-installer.sh
@@ -2,4 +2,9 @@
 set -o errexit
 set -o nounset
 
+$HOME/XIN/bin/stop.sh || echo "Could not stop running node"
+# pkill -9 "xin.runtime.mode=desktop"
+rm -rf $HOME/XIN;
+rm -rf $HOME/.xin;
+
 java -jar build/distributions/iep-node-installer.jar
