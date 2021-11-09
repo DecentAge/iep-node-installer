022ee81c4204ef3f8d084078f8d75b8b3a5094a3
DecentAgeCoder <marco.oesch@decentage.com>
2021-11-09 13:19:34 +0100

    create dmg for mac instead of app

--- src/main/resources/create-mac-pkg.sh
+++ src/main/resources/create-mac-pkg.sh
@@ -7,8 +7,30 @@ echo "BUILD_DIR=${BUILD_DIR}"
 
 #rm -rf ${BUILD_DIR}/build/distributions/iep-node.app
 
-(cd ${BUILD_DIR}/izpack-utils/utils/wrappers/izpack2app && \
-python3 izpack2app.py \
-${BUILD_DIR}/distributions/iep-node-installer.jar \
-${BUILD_DIR}/distributions/iep-node.app)
-sed -i 's/1.4\+/11/' ${BUILD_DIR}/distributions/iep-node.app/Contents/Info.plist
+which java
+
+JAVA_HOME="$(cd $(dirname $(readlink -f $(which java)))/..; pwd)"
+echo JAVA_HOME=$JAVA_HOME
+$JAVA_HOME/bin/javapackager -deploy \
+-outdir . \
+-outfile ardor-client \
+-name ardor-installer \
+-width 34 \
+-height 43 \
+-native dmg \
+-srcfiles iep-node.jar \
+-appclass com.izforge.izpack.installer.bootstrap.Installer \
+-v \
+-Bmac.category=Business \
+-Bmac.CFBundleIdentifier=org.ardor.client.installer \
+-Bmac.CFBundleName=Ardor-Installer \
+-Bmac.CFBundleVersion=${MACVERSION} \
+-BappVersion=${MACVERSION} \
+-Bicon=installer/AppIcon.icns > installer/javapackager.log 2>&1
+
+
+#(cd ${BUILD_DIR}/izpack-utils/utils/wrappers/izpack2app && \
+#python3 izpack2app.py \
+#${BUILD_DIR}/distributions/iep-node-installer.jar \
+#${BUILD_DIR}/distributions/iep-node.app)
+#sed -i 's/1.4\+/11/' ${BUILD_DIR}/distributions/iep-node.app/Contents/Info.plist
