c2b202c814d3ced439e2c98c1b9c41a86271e1dc
DecentAgeCoder <marco.oesch@decentage.com>
2021-11-11 15:45:20 +0100

    Set ports for testnet and mainnet

--- run-linux-installer.sh
+++ run-linux-installer.sh
@@ -3,4 +3,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 export BASE_DIR=$(cd "$(dirname "$BASH_SOURCE")" && pwd -P);
+chmod ug+x ${BASE_DIR}/build/distributions/iep-node-installer.run
 ${BASE_DIR}/build/distributions/iep-node-installer.run
--- src/main/java/xin/installer/UpdateNodePropertiesAction.java
+++ src/main/java/xin/installer/UpdateNodePropertiesAction.java
@@ -9,20 +9,29 @@ import com.izforge.izpack.data.PanelAction;
 
 public class UpdateNodePropertiesAction implements PanelAction {
 
-    private static final Logger logger = Logger.getLogger(UpdateNodePropertiesAction.class.getName());	
-    public static final String FILE_PATH = "conf/nxt-installer.properties";	
-    
-    
+	private static final Logger logger = Logger.getLogger(UpdateNodePropertiesAction.class.getName());
+	public static final String FILE_PATH = "conf/nxt-installer.properties";
+
 	public UpdateNodePropertiesAction() {
 	}
 
 	@Override
 	public void initialize(PanelActionConfiguration configuration) {
 		logger.info("Executing initialize");
-	}	
-	
+	}
+
 	@Override
-	public void executeAction(InstallData adata, AbstractUIHandler handler) {
-		logger.info("Executing executeAction");
+	public void executeAction(InstallData installData, AbstractUIHandler handler) {
+
+		String targetEnv = installData.getVariable("iep.installer.targetEnv");
+
+		if ("testnet".equals(targetEnv)) {
+			installData.setVariable("iep.installer.apiServerPort", "9876");
+			installData.setVariable("iep.installer.peerPort", "8776");
+		} else if ("mainnet".equals(targetEnv)) {
+			installData.setVariable("iep.installer.apiServerPort", "23457");
+			installData.setVariable("iep.installer.peerPort", "23456");
+		}
+		logger.info("Executing executeAction with targetEnv "+targetEnv);
 	}
 }
